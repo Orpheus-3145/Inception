8be646195521cc48df06c92fcef14c151394de37
francesco <fra@faru.42.fr>
2024-09-20 10:57:38 -0500

    switching to intra repo

--- makefile
+++ makefile
@@ -2,12 +2,28 @@ COMPOSE=docker compose -f srcs/docker-compose.yaml
 VOL_WP_PATH = ${HOME}/data/wp
 VOL_DB_PATH = ${HOME}/data/db
 
+SINGLE_SERV = ''
+
 all: build
 
-run: 
+run:
 	@mkdir -p $(VOL_WP_PATH)
 	@mkdir -p $(VOL_DB_PATH)
 	@${COMPOSE} up -d
+
+run_s:
+	@mkdir -p $(VOL_WP_PATH)
+	@mkdir -p $(VOL_DB_PATH)
+	@${COMPOSE} up -d $(SINGLE_SERV)
+
+run_db:
+	@$(MAKE) run_s SINGLE_SERV='mariadb'
+
+run_nginx:
+	@$(MAKE) run_s SINGLE_SERV='nginx'
+
+run_wp:
+	@$(MAKE) run_s SINGLE_SERV='wordpress'
 
 build:
 	@mkdir -p $(VOL_WP_PATH)
@@ -26,11 +42,13 @@ re: fclean run
 down:
 	@${COMPOSE} down
 
-prune:
-	@docker system prune -a
-
 clean:
 	@${COMPOSE} down -v --rmi all
+
+prune:
+	@docker system prune -a
+	@rm -rf $(VOL_DB_PATH)/*
+	@rm -rf $(VOL_WP_PATH)/*
 
 fclean: clean prune
 
--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-if [ ! -e /etc/php/7.4/.php_done ]; then
-    cd /var/www/html
-	echo "=================================$DOMAIN_NAME============================================"
-    chown -R www-data:www-data /var/www/html	
+cd /var/www/html/"$DOMAIN_NAME"
+if [ ! -e .wordpress_setup_done ]; then
+
+    chown -R www-data:www-data /var/www/html/"$DOMAIN_NAME"
     # Download the WordPress core files
 	wp core download --allow-root
 	# Create the WordPress configuration file with the provided database credentials and host
-	wp config create --allow-root --dbname=$DB_NAME --dbuser=$WP_USER --dbpass=$WP_USER_PWD --dbhost=$DB_HOST:$DB_PORT
-	# Install WordPress with the provided site information and admin credentials
-	wp core install --allow-root --url=$DOMAIN_NAME --title="inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email
-	# Create a new WordPress user with the provided credentials and role
-	wp user create --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PWD --role=author
+	wp config create	--allow-root \
+						--dbname="$DB_NAME" \
+						--dbuser="$WP_USER" \
+						--dbpass="$WP_USER_PWD" \
+						--dbhost="$DB_HOST:$DB_PORT"
 
-    touch /etc/php/7.4/.php_done
+	# Install WordPress with the provided site information and admin credentials
+	wp core install	--allow-root \
+					--url="https://$DOMAIN_NAME/" \
+					--title="inception" \
+					--admin_user="$WP_ADMIN_USER" \
+					--admin_password="$WP_ADMIN_PWD" \
+					--admin_email="$WP_ADMIN_EMAIL" \
+					--skip-email
+
+	# Create a new WordPress user with the provided credentials and role
+	wp user create	"$WP_USER" "$WP_USER_EMAIL" \
+					--allow-root  \
+					--user_pass="$WP_USER_PWD" \
+					--role=author
+
+    touch .wordpress_setup_done
 fi
 
 exec "$@"
